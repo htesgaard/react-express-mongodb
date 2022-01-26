2944afc5eff4f1490962cbd7b443a94007eee3d6
Helge Tesgaard <helge.tesgaard@gmail.com>
2022-01-26 20:44:23 +0100

    Add work

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,23 +2,15 @@ const express = require("express");
 const serverResponses = require("../utils/helpers/responses");
 const messages = require("../config/messages");
 const { Todo } = require("../models/todos/todo");
+const { Client } = require("../models/clients/client");
 
 const routes = (app) => {
   const router = express.Router();
 
-  router.post("/clients", (req, res) => {
-    const client = new Client({
-      clientID: req.body.clientID,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      streetName: req.body.streetName,
-      city: req.body.city
-    });
-
-    client
-      .save()
-      .then((result) => {
-        serverResponses.sendSuccess(res, messages.SUCCESSFUL, result);
+  router.get("/clients", (req, res) => {
+    Client.find({}, { __v: 0 })
+      .then((clients) => {
+        serverResponses.sendSuccess(res, messages.SUCCESSFUL, clients);
       })
       .catch((e) => {
         serverResponses.sendError(res, messages.BAD_REQUEST, e);
@@ -41,6 +33,16 @@ const routes = (app) => {
   });
 
   router.get("/", (req, res) => {
+    Todo.find({}, { __v: 0 })
+      .then((todos) => {
+        serverResponses.sendSuccess(res, messages.SUCCESSFUL, todos);
+      })
+      .catch((e) => {
+        serverResponses.sendError(res, messages.BAD_REQUEST, e);
+      });
+  });
+
+  router.get("/todos", (req, res) => {
     Todo.find({}, { __v: 0 })
       .then((todos) => {
         serverResponses.sendSuccess(res, messages.SUCCESSFUL, todos);
